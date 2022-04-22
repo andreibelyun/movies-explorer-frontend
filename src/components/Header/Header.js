0ec632f69933e6fd5bc4017e86a3624ef4d2a9ea
Andrey Belyun <andreibelyun@yandex.com>
2022-04-22 15:38:09 +0300

    fix: display header only on the necessary routes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,13 +5,15 @@ import Navigation from '../Navigation/Navigation';
 
 export default function Header() {
 
-    const location = useLocation();
-    const headerClass = location.pathname === '/' ? 'header header_black' : 'header';
+    const currentPath = useLocation().pathname;
+    const headerClass = currentPath === '/' ? 'header header_black' : 'header';
+    const needHeaderEndpoints = ['/', '/movies', '/saved-movies', '/profile'];
+    const isVisible = needHeaderEndpoints.includes(currentPath);
 
-    return (
+    if (isVisible) return (
         <header className={headerClass}>
             <Link to='/'>
-                <img className='header__logo interactive-button' alt='Логотип' src={logo}/>
+                <img className='header__logo interactive-button' alt='Логотип' src={logo} />
             </Link>
             <Navigation />
         </header>
