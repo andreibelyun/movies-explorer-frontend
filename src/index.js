d53b33c1660842a09c691505981818e0abdc93ef
Andrey Belyun <andreibelyun@yandex.com>
2023-01-07 16:18:34 +0300

    fix: delete extra links

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './components/App/App';
 import { BrowserRouter } from 'react-router-dom';
 
